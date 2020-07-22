24747954245746081b0e65494b8ce538af42ca47
riyazthk <er.riyaz2507@gmail.com>
2020-07-22 16:07:47 +0530

    [Riyaz]ADDED:To test a user validation for first name

--- src/test/java/com/registration/UserValidatorTest.java
+++ src/test/java/com/registration/UserValidatorTest.java
@@ -1,4 +1,20 @@
 package com.registration;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserValidatorTest {
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        UserValidator userValidator = new UserValidator();
+        boolean result=userValidator.validateFirstName("Riyaz");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void givenFirstName_WhenShort_ShouldReturnFalse() {
+        UserValidator userValidator = new UserValidator();
+        boolean result=userValidator.validateFirstName("riyaz");
+        Assert.assertEquals(true,result);
+    }
 }
