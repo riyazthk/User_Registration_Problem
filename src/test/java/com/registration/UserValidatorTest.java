84d1ce912404f83501d9ff07c0912c6adb8c8ccd
riyazthk <er.riyaz2507@gmail.com>
2020-07-22 20:47:47 +0530

    [Riyaz]ADDED:To check test cases for email id

--- src/test/java/com/registration/UserValidatorTest.java
+++ src/test/java/com/registration/UserValidatorTest.java
@@ -26,9 +26,24 @@ public class UserValidatorTest {
     }
 
     @Test
-    public void givenLastName_WhenProper_ShouldReturnFalse() {
+    public void givenLastName_WhenShort_ShouldReturnFalse() {
         UserValidator userValidator = new UserValidator();
         boolean result = userValidator.validateLastName("ahamed");
         Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void givenEmailId_WhenProper_ShouldReturnTrue() {
+        UserValidator userValidator = new UserValidator();
+        boolean result=userValidator.validateEmailId("riyaz.ahamed2507@gmail.com.in");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void givenEmailId_WhenShort_ShouldReturnFalse() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateEmailId("riyaz.ahamed@.gmail.com");
+        Assert.assertEquals(true,result);
+
     }
 }
