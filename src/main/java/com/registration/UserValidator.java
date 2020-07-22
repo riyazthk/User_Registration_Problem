44d1bf2f45d993ef88363e8ec7689baf5d3730c3
Riyaz <40356351+riyazthk@users.noreply.github.com>
2020-07-22 16:23:02 +0530

    Update UserValidator.java

--- src/main/java/com/registration/UserValidator.java
+++ src/main/java/com/registration/UserValidator.java
@@ -1,4 +1,10 @@
 package com.registration;
 
 public class UserValidator {
+  private static final String user_Name= "^[A-Z][a-z]{2,}$";
+
+    public boolean validateFirstName(String fName) {
+        Pattern pattern=Pattern.compile(user_Name);
+        return pattern.matcher(fName).matches();
+    }
 }
