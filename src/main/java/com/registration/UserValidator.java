dfe77fc8bd3ed55735356164304eddc099d2d9a4
riyazthk <er.riyaz2507@gmail.com>
2020-07-22 16:14:44 +0530

    [Riyaz]ADDED:To write a code user validation using regex

--- src/main/java/com/registration/UserValidator.java
+++ src/main/java/com/registration/UserValidator.java
@@ -1,4 +1,12 @@
 package com.registration;
 
+import java.util.regex.Pattern;
+
 public class UserValidator {
+    private static final String user_Name= "^[A-Z][a-z]{2,}$";
+
+    public boolean validateFirstName(String fName) {
+        Pattern pattern=Pattern.compile(user_Name);
+        return pattern.matcher(fName).matches();
+    }
 }
