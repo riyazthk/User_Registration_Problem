83504f8bee862537f3059e400482e74943c9947c
riyazthk <er.riyaz2507@gmail.com>
2020-07-22 21:48:28 +0530

    [Riyaz]ADDED:To write a code for password contain atleast 8 character

--- src/main/java/com/registration/UserValidator.java
+++ src/main/java/com/registration/UserValidator.java
@@ -7,6 +7,7 @@ public class UserValidator {
     private static final String user_Name= "^[A-Z][a-z]{2,}$";
     private static final String email_Id= "^([0-9a-zA-Z/-/_+.]*[@][^.][a-zA-Z]*.[a-z]{2,4}(.[a-z]{2})?)$";
     private static final String ph_Num= "^91[ ][0-9]{10}$";
+    private static final String password="^[0-9A-z]{8,}$";
 
 
     public boolean validateFirstName(String fName) {
@@ -21,13 +22,18 @@ public class UserValidator {
     }
 
     public boolean validateEmailId(String emailId) {
-    Pattern pattern=Pattern.compile(email_Id);
-    return pattern.matcher(emailId).matches();
+        Pattern pattern=Pattern.compile(email_Id);
+        return pattern.matcher(emailId).matches();
     }
 
 
     public boolean validatePhnum(String phNum) {
-    Pattern pattern=Pattern.compile(ph_Num);
-    return pattern.matcher(phNum).matches();
+        Pattern pattern=Pattern.compile(ph_Num);
+        return pattern.matcher(phNum).matches();
+    }
+
+    public boolean ValidatePassword(String pass) {
+        Pattern pattern=Pattern.compile(password);
+        return  pattern.matcher(pass).matches();
     }
 }
